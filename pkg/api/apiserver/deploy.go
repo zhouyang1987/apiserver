e83a0c8e1e019585bcb8ee9113207b3149045b2e
huangjiasingle <449264675@qq.com>
2017-06-19 16:48:57 +0800

    update deploy interface

--- pkg/api/apiserver/deploy.go
+++ pkg/api/apiserver/deploy.go
@@ -3,3 +3,7 @@ package apiserver
 func InsertDeploy(deploy *Deploy) error {
 	return db.Create(deploy).Error
 }
+
+func InsertProjectConfig(config *ProjectConfig) error {
+	return db.Create(config).Error
+}
--- pkg/apis/apiserver/deploy.go
+++ pkg/apis/apiserver/deploy.go
@@ -2,10 +2,13 @@ package apiserver
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"apiserver/pkg/api/apiserver"
+	"apiserver/pkg/configz"
 	r "apiserver/pkg/router"
+	httpUtil "apiserver/pkg/util/registry"
 )
 
 func CreatDeploy(request *http.Request) (string, interface{}) {
@@ -14,11 +17,32 @@ func CreatDeploy(request *http.Request) (string, interface{}) {
 		return r.StatusInternalServerError, err
 	}
 
+	tranport := httpUtil.GetHttpTransport(false)
+	url := configz.GetString("apiserver", "getConfigUrl", "http://localhost:8080/projects/%s/configs")
+	client := &http.Client{Transport: tranport}
+
 	for _, deploy := range deploys {
 		if err = apiserver.InsertDeploy(deploy); err != nil {
 			return r.StatusInternalServerError, err
 		}
+		for _, item := range deploy.Items {
+			projectConfigs := []*apiserver.ProjectConfig{}
+			url = fmt.Sprintf(url, item.ProjectId)
+			res, err := client.Get(url)
+			if err != nil {
+				return r.StatusInternalServerError, fmt.Sprintf("get project [%s] config err:%v", item.ProjectName, err.Error())
+			}
+			if err = json.NewDecoder(res.Body).Decode(&projectConfigs); err != nil {
+				return r.StatusInternalServerError, err
+			}
+			for _, projectConfig := range projectConfigs {
+				if err = apiserver.InsertProjectConfig(projectConfig); err != nil {
+					return r.StatusInternalServerError, err
+				}
+			}
+		}
 	}
+
 	return r.StatusCreated, "ok"
 }
 
@@ -29,3 +53,5 @@ func validateDeploy(request *http.Request) ([]*apiserver.Deploy, error) {
 	}
 	return deploys, nil
 }
+
+//反馈接口待定
